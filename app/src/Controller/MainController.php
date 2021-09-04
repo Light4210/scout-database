60c47cde1634d151bd0a8b8a4079e4ef71d46cd7
Vitalii <tarnavskij2002@gmail.com>
2021-09-04 18:49:44 +0300

    routes

--- app/src/Controller/MainController.php
+++ app/src/Controller/MainController.php
@@ -6,13 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends AbstractController
+class MainController extends AbstractController
 {
     #[Route('/default', name: 'default')]
     public function index(): Response
     {
+        $em = $this->getDoctrine()->getManager();
+        $em->getConnection()->connect();
+        $connected = $em->getConnection()->isConnected();
+        dd($connected);
         return $this->render('default/base.html.twig', [
-            'controller_name' => 'DefaultController',
+            'controller_name' => 'MainController',
         ]);
     }
 }
---
+++ app/src/Entity/Struct.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StructRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=StructRepository::class)
+ */
+class Struct
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adress;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\OneToOne(targetEntity="App\Entity\User", inversedBy="Struct")
+     */
+    private $shef_id;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $latitude;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $longitude;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getAdress(): ?string
+    {
+        return $this->adress;
+    }
+
+    public function setAdress(string $adress): self
+    {
+        $this->adress = $adress;
+
+        return $this;
+    }
+
+    public function getShefId(): ?int
+    {
+        return $this->shef_id;
+    }
+
+    public function setShefId(int $shef_id): self
+    {
+        $this->shef_id = $shef_id;
+
+        return $this;
+    }
+
+    public function getLatitude(): ?int
+    {
+        return $this->latitude;
+    }
+
+    public function setLatitude(?int $latitude): self
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    public function getLongitude(): ?int
+    {
+        return $this->longitude;
+    }
+
+    public function setLongitude(?int $longitude): self
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+}
