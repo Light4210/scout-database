acd9a124e5a2c0ace5fe8cd4d846d21b2bed8b49
vitar <tarnavskij2002@gmail.com>
2022-01-12 19:48:38 +0200

    added promotion on edit user page

---
+++ app/src/Entity/Notification.php
@@ -0,0 +1,232 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NotificationRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass=NotificationRepository::class)
+ */
+class Notification
+{
+    const TYPE_TRANSFER = 'transfer';
+
+    const STATUS_APPROVED = 'approved';
+    const STATUS_DECLINED = 'declined';
+    const STATUS_PENDING = 'pending';
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $title;
+
+    /**
+     * @ORM\Column(type="string", length=500, nullable=true)
+     */
+    private string $content;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private string $type;
+
+    /**
+     * @ORM\Column(type="string", length=50, nullable=false)
+     */
+    private string $status;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private bool $isRead;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="notifications")
+     */
+    private User $targetUser;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private User $fromUser;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private User $toUser;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=false)
+     */
+    private $created_at;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updated_at;
+
+    /**
+     * @param $title
+     * @param $content
+     * @param $type
+     * @param $targetUser
+     * @param $fromUser
+     * @param $toUser
+     */
+
+    public function __construct(string $title, string $content, string $type, User|UserInterface $targetUser, User|UserInterface $fromUser, User|UserInterface $toUser)
+    {
+        $this->title = $title;
+        $this->content = $content;
+        $this->type = $type;
+        $this->targetUser = $targetUser;
+        $this->fromUser = $fromUser;
+        $this->toUser = $toUser;
+        $this->isRead = false;
+        $this->status = self::STATUS_PENDING;
+        $this->setCreatedAt(new \DateTimeImmutable());
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getIsRead(): ?bool
+    {
+        return $this->isRead;
+    }
+
+    public function setIsRead(bool $isRead): self
+    {
+        $this->isRead = $isRead;
+
+        return $this;
+    }
+
+    public function getTargetUser(): ?User
+    {
+        return $this->targetUser;
+    }
+
+    public function setTargetUser(?User $targetUser): self
+    {
+        $this->targetUser = $targetUser;
+
+        return $this;
+    }
+
+    public function getFromUser(): ?User
+    {
+        return $this->fromUser;
+    }
+
+    public function setFromUser(?User $fromUser): self
+    {
+        $this->fromUser = $fromUser;
+
+        return $this;
+    }
+
+    public function getToUser(): ?User
+    {
+        return $this->toUser;
+    }
+
+    public function setToUser(?User $toUser): self
+    {
+        $this->toUser = $toUser;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status): void
+    {
+        $this->status = $status;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(?\DateTimeImmutable $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+
+
+}
