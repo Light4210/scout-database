a9233fd72068a0fc7b93973dda438fe89692ae20
vitar <tarnavskij2002@gmail.com>
2022-03-23 21:52:19 +0200

    general user list improvement

--- app/src/Controller/User/ListUserController.php
+++ app/src/Controller/User/ListUserController.php
@@ -14,31 +14,7 @@ class ListUserController extends AbstractController
     public function __invoke(Request $request, EntityManagerInterface $entityManager, Security $security, RedirectService $redirectService)
     {
         /** @var User $currentUser */
-        $currentUser = $security->getUser();
-
-        $role = $request->attributes->get('role');
-
-        if (User::ACTIVE_MINISTRY[$currentUser->getMinistry()]['access'] < User::PRIORITY_NATIONAL_COUNCIL) {
-            return $redirectService->redirectWithPopup(
-                RedirectService::MESSAGE_TYPE['fail'],
-                RedirectService::MESSAGE_TEXT['ACCESS_DENIED'],
-                'index',
-                []
-            );
-        }
-
-        if ($role == 'all') {
-            $users = $entityManager->getRepository(User::class)->findAll();
-        } else if (array_key_exists($role, [User::ROLE_SCOUT, User::ROLE_TRAVELLER, User::ROLE_WOLVES])) {
-            $users = $entityManager->getRepository(User::class)->findBy(['role' => $role]);
-        } else {
-            return $redirectService->redirectWithPopup(
-                RedirectService::MESSAGE_TYPE['fail'],
-                RedirectService::MESSAGE_TEXT['WRONG_ROLE_NAME'],
-                'index',
-                []
-            );
-        }
+        $users = $entityManager->getRepository(User::class)->findBy(['role' => User::ROLE_TRAVELLER]);
 
         return $this->render('admin/user/user-list.html.twig', ['users' => $users]);
     }
