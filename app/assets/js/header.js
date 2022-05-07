16107d4341bd9a9dbcec775b3afc672d16a6dd80
vitar <tarnavskij2002@gmail.com>
2022-05-07 21:42:01 +0300

    header menu + deploy fix

--- app/assets/js/header.js
+++ app/assets/js/header.js
@@ -2,7 +2,6 @@ let burger = document.getElementById('burger')
 let menu = document.getElementById('menu-list')
 alert('Hi actions is work')
 
-
 burger.addEventListener('click', function () {
     burger.classList.toggle('open')
     if(burger.classList.contains('open')){
