a937f94cf3755c0cf7422f8c15f81a4b68d4b28e
Vitalik <tarnavskij2002@gmail.com>
2022-08-15 21:55:01 +0300

    add games

--- app/assets/js/header.js
+++ app/assets/js/header.js
@@ -5,7 +5,9 @@ burger.addEventListener('click', function () {
     burger.classList.toggle('open')
     if(burger.classList.contains('open')){
         menu.style.opacity = "1"
+        menu.style.display = "block"
     } else {
         menu.style.opacity = "0"
+        menu.style.display = "none"
     }
 })
